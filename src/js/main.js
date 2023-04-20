2070803906cd5f3b6710ea62c40c8d0a15116bed
mumu <zramj314@gmail.com>
2023-04-20 23:38:43 +0800

    Add - 點擊標籤active, 標籤篩選功能

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('.topic-nav-wrap a').click(function() {
+        event.preventDefault();
+        $('.topic-nav-wrap a').removeClass('tag-active');
+        $(this).addClass('tag-active');
+    });
+
+
+// 當點擊沒有 data-topic-filter 屬性的篩選器時，顯示所有 <li> 元素
+  $('li[data-topic-filter=""]').click(function() {
+    $('.main-card li').show();
+  });
+
+  // 當點擊具有 data-topic-filter 屬性的篩選器時，只顯示符合條件的 <li> 元素
+  $('li[data-topic-filter]').click(function() {
+    var topicTag = $(this).data('topic-filter');
+    $('.main-card li').hide();
+    $('.main-card li' + topicTag).show();
+  });
+
+});
