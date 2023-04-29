6525027687d9204d8ef97c177c812a851a4ab8cd
mumu <zramj314@gmail.com>
2023-04-30 00:27:24 +0800

    新增表單驗證外掛
    新增外掛，但尚未完成

---
+++ src/js/form-validate.js
@@ -0,0 +1,57 @@
+$(function(){
+    $('#form').validate({
+        /* 常用檢測屬性
+        required:必填
+        noSpace:空白
+        minlength:最小長度
+        maxlength:最大長度
+        email:信箱格式
+        number:數字格式
+        url:網址格式https://www.minwt.com
+        */
+        onkeyup: function(element, event) {
+            //去除左側空白
+            var value = this.elementValue(element).replace(/^\s+/g, "");
+            $(element).val(value);
+        },
+        rules: {
+            fname: {
+                required: true
+            },
+            phone:{
+                required: true,
+                minlength: 8,
+                        number: true
+            },
+            address: 'required',
+            url:{
+                url: true
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            fname: {
+                required:'必填'
+            },
+            phone: {
+                required:'必填',
+                minlength:'不得少於8位',
+                number:'電話需為數字'
+            },
+            address: '必填',
+            email: {
+                required:'必填',
+                email:'Email格式不正確'
+            },
+            url:'網址格式不正確'        
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+
+
+});
